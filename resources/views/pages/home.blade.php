f0f08375e25deed4bd78d625e726c6fd952eed2d
Devendra Verma <devendrakumar.vspl@gmail.com>
2026-03-05 06:58:02 +0000

    update images

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -301,7 +301,7 @@
 
             .tb-cats-grid {
                 display: grid;
-                grid-template-columns: repeat(6, 1fr);
+                grid-template-columns: repeat(5, 1fr);
                 gap: 16px;
             }
 
@@ -1745,7 +1745,7 @@
             }
 
             .tb-splitcoll-img.big {
-                height: 380px;
+                height: 444px;
                 background-image: url("{{ asset('images/milk-vans.webp') }}");
             }
 
